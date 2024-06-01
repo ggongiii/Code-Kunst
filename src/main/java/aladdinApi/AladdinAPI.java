8274f6f0a40c6e66b7a8b9ad175363911e4f4f40
LeeDoHyeong <ehgud1005@gmail.com>
2024-06-01 16:38:56 +0900

    [수정] 미세수정

---
+++ src/main/java/aladdinApi/AladdinAPI.java
@@ -0,0 +1,12 @@
+package aladdinApi;
+
+public class AladdinAPI {
+
+	private static final String apiurl = "http://www.aladdin.co.kr/ttb/api/ItemList.aspx?";
+	
+	public void bookInfo() {
+		String book[] = {};
+		
+	}
+	
+}
---
+++ src/main/java/aladdinApi/TestInsertBook.java
@@ -0,0 +1,26 @@
+package aladdinApi;
+import java.util.Date;
+
+public class TestInsertBook {
+
+	//테스트용
+    public static void main(String[] args) {
+        BookDTO book = new BookDTO();
+        book.setBook_id(1);
+        book.setBook_name("이도형");
+        book.setIsbn("123456789");
+        book.setBook_price(1999);
+        book.setPublish_date("2024/02/02");
+        book.setBook_pic("sample.jpg");
+        book.setBook_info("이도형이쓴책");
+        book.setInventory(100);
+        book.setPublisher("퍼블리셔");
+        book.setBook_category("코딩");
+        book.setAuthor("책저자");
+
+        BookDAO bookDAO = new BookDAO();
+        bookDAO.insertBook(book);
+
+        System.out.println("성공");
+    }
+}
